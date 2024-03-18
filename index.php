5ed89cf10d8c8da36071a6ac275970f347455606
ravishankarsaini <ravishanker21@gmail.com>
2024-03-18 17:19:29 +0530

    names update

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-// Prompt user to enter two numbers
-$num1 = readline("Enter the first number: ");
-$num2 = readline("Enter the second number: ");
-
-// Convert inputs to integers
-$num1 = (int) $num1;
-$num2 = (int) $num2;
-
-// Add the numbers and display the result
-echo $num1 + $num2 . ": ";
+echo "JAI SHRI RAM";
+echo "JAI HANUMAAN";
 ?>
